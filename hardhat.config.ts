6cc47b8fe424bcb9254793911103300bd5136319
Hoang Lai Nhat <nhathoang8c01@gmail.com>
2024-01-19 17:06:43 +0700

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,24 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-import '@nomicfoundation/hardhat-chai-matchers';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-solhint';
-import '@openzeppelin/hardhat-upgrades';
-import '@typechain/hardhat';
-import 'dotenv/config';
-import 'hardhat-deploy';
-import 'solidity-coverage';
-import 'hardhat-gas-reporter';
-import 'hardhat-contract-sizer';
-import 'hardhat-abi-exporter';
-import { HardhatUserConfig } from 'hardhat/config';
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-solhint";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import "dotenv/config";
+import "hardhat-deploy";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "hardhat-contract-sizer";
+import "hardhat-abi-exporter";
+import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.13',
+    version: "0.8.13",
     settings: {
       optimizer: {
         enabled: true,
@@ -25,24 +26,38 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: 'bsctest',
+  defaultNetwork: "bsctest",
   networks: {
     hardhat: {},
     bsctest: {
-      url: 'https://data-seed-prebsc-1-s1.bnbchain.org:8545',
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: ['8e6e405b79265705b91d371bb65d4145825be9c7fb7aef653aeb31d05d60a6b4'],
+      accounts: [process.env.PRIVATE_KEY || "import-key"],
+    },
+    mantleTest: {
+      url: "https://rpc.testnet.mantle.xyz",
+      accounts: [process.env.PRIVATE_KEY || "import-key"],
     },
   },
   etherscan: {
-    apiKey: 'F38BQ789E5PV69EABD8C1QUMAISZQRIG4D',
+    apiKey: process.env.BSCSCAN_API_KEY || "import-key",
+    customChains: [
+      {
+        network: "mantleTest",
+        chainId: 5001,
+        urls: {
+          apiURL: "https://explorer.testnet.mantle.xyz/api",
+          browserURL: "https://explorer.testnet.mantle.xyz",
+        },
+      },
+    ],
   },
   contractSizer: {
     runOnCompile: true,
   },
   abiExporter: {
-    path: './abis',
+    path: "./abis",
     runOnCompile: true,
     clear: true,
     flat: true,
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,35 @@
-// const { ethers, upgrades } = require("hardhat");
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(">> deployer: ", deployer.address);
 
+  // "quote" => USDC
+  // "sanactions"?, "sequencer"?, get price with oracle?
+
   // const OffchainBook = await ethers.getContractFactory('OffchainBook');
   // const offchainBook = await upgrades.deployProxy(OffchainBook);
   // await offchainBook.deployed();
 
+  // FEE CALCULATOR
   const FeeCalculator = await ethers.getContractFactory("FeeCalculator");
   const feeCalculator = await FeeCalculator.deploy();
   await feeCalculator.deployed();
+  // ========================
 
+  // CLEARING HOUSE LIQ
   const ClearinghouseLiq = await ethers.getContractFactory("ClearinghouseLiq");
   const clearinghouseLiq = await ClearinghouseLiq.deploy();
   await clearinghouseLiq.deployed();
+  // ========================
 
+  // CLEARING HOUSE
   const Clearinghouse = await ethers.getContractFactory("Clearinghouse");
   const clearinghouse = await upgrades.deployProxy(
     Clearinghouse,
     [
-      // "", // endpoint
-      // "", // quote ???
-      feeCalculator.address, // xx
-      feeCalculator.address, // xx
+      "0x0000000000000000000000000000000000000000", // endpoint -> 'll set later
+      "0x0000000000000000000000000000000000000000", // quote ??? -> USDC
       feeCalculator.address,
       clearinghouseLiq.address,
     ],
@@ -34,45 +39,54 @@ async function main() {
     }
   );
   await clearinghouse.deployed();
+  // ========================
 
+  // ENDPOINT
   const Endpoint = await ethers.getContractFactory("Endpoint");
   const endpoint = await upgrades.deployProxy(Endpoint, [
-    "", // sanactions address,
-    "", // sequencer address,
+    "0x0000000000000000000000000000000000000000", // sanactions address,
+    "0x0000000000000000000000000000000000000000", // sequencer address,
     clearinghouse.address, // clearinghouse address
-    72000, // slowModeTimeout
+    72000, // slowModeTimeout - if place order with CLOB failed -> AMM
     0, // time {perpTime, spotTime}??
-    [], // price[a-spot, a-perp, b-spot, b-perp, ...]
+    [4, 3, 32, 22, 12, 222], // price[a-spot, a-perp, b-spot, b-perp, ...] -> healthGroup
   ]);
   await endpoint.deployed();
+  // set endpoint for clearing house
+  await clearinghouse.setEndpoint(endpoint.address);
+  // ========================
 
+  // SPOT ENGINE
   const SpotEngine = await ethers.getContractFactory("SpotEngine");
   const spotEngine = await upgrades.deployProxy(SpotEngine, [
     clearinghouse.address, // clearinghouse address
-    "", // quote address
+    "0x0000000000000000000000000000000000000000", // quote address
     endpoint.address, // endpoint address
-    "", // admin address
+    deployer.address, // admin address
     feeCalculator.address,
   ]);
   await spotEngine.deployed();
+  // ========================
 
+  // PERP ENGINE
   const PerpEngine = await ethers.getContractFactory("PerpEngine");
   const perpEngine = await upgrades.deployProxy(PerpEngine, [
     clearinghouse.address, // clearinghouse address
-    "", // quote address
+    "0x0000000000000000000000000000000000000000", // quote address
     endpoint.address, // endpoint address
-    "", // admin address
+    deployer.address, // admin address
     feeCalculator.address,
   ]);
   await perpEngine.deployed();
+  // ========================
 
-  console.log("Address");
+  console.log("Addresses");
   console.log(">> FeeCalculator: ", feeCalculator.address);
   console.log(">> ClearinghouseLiq: ", clearinghouseLiq.address);
   console.log(">> Clearinghouse: ", clearinghouse.address);
   console.log(">> Endpoint: ", endpoint.address);
   console.log(">> SpotEngine: ", spotEngine.address);
-  console.log(">> SpotEngine: ", perpEngine.address);
+  console.log(">> PerpEngine: ", perpEngine.address);
   // console.log(">> OffchainBook: ", offchainBook.address);
 }
 
